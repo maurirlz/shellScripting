67d564d059e63452bc04177158e74079db1d2f8c
maurirlz <mauribenitez95@gmail.com>
2021-08-22 00:04:28 -0300

    test + if/else statements

--- scriptingBasics/main.sh
+++ scriptingBasics/main.sh
@@ -5,8 +5,22 @@
 targetContainer="$1";
 targetDir="$2";
 finalPath="$targetDir/$targetContainer.csv"
+errorStatusCode=1;
+
+if [[ ! "$1" ]]; then
+    echo "Error: missing parameter: container name"
+    exit $errorStatusCode;
+fi
+
+if [[ ! $2 ]]; then
+  targetDir="$HOME/test";
+fi
 
 mkdir -pv -- "$targetDir";
-grep -- "$targetContainer" ./shipments.csv > "${finalPath}";
 
-echo "Wrote report @ $finalPath";
+ if grep -- "$targetContainer" ./shipments.csv > "${finalPath}"
+ then
+   echo "Wrote report @ $finalPath";
+ else
+   echo "No container found";
+ fi
